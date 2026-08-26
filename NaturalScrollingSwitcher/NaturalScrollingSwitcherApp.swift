171ac62a1d43763139c4af0014ceed7909b89a12
Akeri <asd0014a@gmail.com>
2026-08-26 17:26:21 +0900

    Initial Commit

---
+++ NaturalScrollingSwitcher/NaturalScrollingSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalScrollingSwitcherApp.swift
+//  NaturalScrollingSwitcher
+//
+//  Created by Akeri on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct NaturalScrollingSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
